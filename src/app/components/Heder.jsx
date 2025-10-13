6dffe7f540c3bf34cf2bb9e95a39fe91728ec884
kali <kali@kali>
2025-10-14 00:35:08 +0300

    Обновление проекта: все изменения

--- src/app/components/Heder.jsx
+++ src/app/components/Heder.jsx
@@ -10,6 +10,21 @@ import {  useState } from "react";
 
 export default function Heder() {
   const [fynts, setfynts] =useState(true)
+  const [nav ,setnav] = useState(false)
+
+   const products = [
+    {img:'/img/image 1.png',name:'Джокер',kateor:'Фантастика'},
+     {img:'/img/image 2 (2).png',name:'сталин',kateor:'трелер'},
+      {img:'/img/image 2.png',name:'хуй',kateor:'Фантастика'},
+       {img:'/img/image 2 (1).png',name:'адам',kateor:'Фантастика'},
+        {img:'/img/image 1.png',name:'Джокер',kateor:'Фантастика'},
+     {img:'/img/image 2 (2).png',name:'сталин',kateor:'трелер'},
+      {img:'/img/image 2.png',name:'хуй',kateor:'Фантастика'},
+       {img:'/img/image 2 (1).png',name:'адам',kateor:'Фантастика'},
+       {img:'/img/image 2 (1).png',name:'адам',kateor:'Фантастика'},
+       
+        
+   ]
 
   const filter = () => {
      if(!fynts){
@@ -23,8 +38,8 @@ export default function Heder() {
     const navigaat = ['media', 'films', 'actors', 'news', 'categories'];
   return (
 <div className=" relative ">
-    <div className="w-[100%]  z-0 h-[1100px] bg-[rgba(30,37,56,1)]">
-    <div className="w-[100%]  z-10 h-[1100px] bg-[url('/img/efe.png')] bg-cover bg-center">
+    <div className="w-[100%]  z-0 h-[100%] bg-[rgba(30,37,56,1)]">
+    <div className="w-[100%]  z-10 h-[100%] bg-[url('/img/efe.png')] bg-cover bg-center">
 
  <div className="flex min-[501px]:hidden">
         {!fynts && (
@@ -37,9 +52,9 @@ export default function Heder() {
       width={125}
       height={125}
       alt="User Avatar"
-      className="w-[125px]  h-[125px] rounded-full shadow-lg"
+      className="w-[125px] min-[400px]:ml-[100px] max-[400px]:ml-[90px] h-[125px] rounded-full shadow-lg"
     />
-    <button onClick={ () => setfynts(!false)}>X</button>
+    <button className=" min-[400px]:ml-[90px] max-[400px]:ml-[70px] text-[25px] text-blue-600" onClick={ () => setfynts(!false)}>X</button>
   </div>
   
 
@@ -152,6 +167,74 @@ export default function Heder() {
    </button>
   </div>
     </div>
+
+
+
+  <div className="flex justify-center  ">
+     <div className="hidden min-[1420px]:flex">
+       <h2 className="text-[38px] text-[#00e132] nosifer-regular">Now at the cinema</h2>
+     </div>
+
+    <div className="hidden min-[1420px]:flex">
+      <div className="w-[60px] mt-[23px] mx-[50px] h-[3px] bg-amber-50"></div>
+    </div>
+   
+<div className="hidden min-[600px]:flex">
+    {['All','action films','Adventures','comedy','fantasy','thrillers','drama'].map((genre, index) => (
+  <a href="#" key={index} className="mx-2 nosifer-regular min-[700px]:text-[13px] max-[700px]:text-[10px] mt-[16px] text-[#73848c] hover:text-[#fbff00] hover:underline transition duration-300">
+    {genre}
+  </a>
+))}
+</div>
+  </div>
+
+<div>
+    <div className="flex justify-center   min-[1200px]:mx-[170px] max-[1200px]:mx-[20px] flex-wrap mt-[50px] min-[500px]:gap-[22px] max-[500px]:gap-[13px]">
+    {(nav ? products :products.slice(0,8)).map((product,index)=> (
+          <div  key={index}>
+
+
+            <Image
+  src={product.img}
+  width={330}
+  height={440}
+  alt={product.name}
+  className=" 2xl:w-[330px] xl:w-[330px] xl:max-[1453px]:w-[202px]  lg:w-[202px] min-[500px]:w-[202px] max-[500px]:w-[160px]   2xl:h-[440px] xl:h-[440px] xl:max-[1453px]:h-[270px] lg:h-[270px]   min-[500px]:h-[270px]  max-[500px]:h-[212px] rounded-[15px] object-cover"
+/>
+<h3>{product.name}</h3>
+<h3>{product.kateor}</h3>
+
+
+        
+
+      </div>
+    )
+  
+
+    )}
+
+    
+  </div>
+   <div className="flex justify-center">
+     {!nav && products.length > 8 && (
+        <button
+          onClick={() => setnav(true)}
+          className="mt-6 px-4 py-2 bg-amber-300 rounded-full"
+        >
+          Показать ещё
+        </button>
+      )}
+   </div>
+  
+</div>
+
+  
+
+
+
+
+
+
    
 
 
